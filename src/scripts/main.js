85c5855dab3e2338e7c91ed7bd000784f3df693d
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-13 21:47:59 +0300

    Implement design: First part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const toggle = document.querySelector('.toggle-action');
+const body = document.body;
+
+function headerToggle() {
+  if (body.classList.contains('_menu-open')) {
+    body.classList.remove('_menu-open');
+  } else {
+    body.classList.add('_menu-open');
+  }
+}
+
+toggle.addEventListener('click', headerToggle());
