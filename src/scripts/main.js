e6395bbf5a4b73dcfacb48264e6aa19f9c9e0b59
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-14 20:00:16 +0300

    Implement design: Second part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const toggle = document.querySelector('.toggle-action');
+const toggle = document.querySelectorAll('.toggle-action');
 const body = document.body;
 
 function headerToggle() {
+  console.log('click');
+  
   if (body.classList.contains('_menu-open')) {
     body.classList.remove('_menu-open');
   } else {
@@ -11,4 +13,6 @@ function headerToggle() {
   }
 }
 
-toggle.addEventListener('click', headerToggle());
+toggle.forEach((item) => {
+  item.addEventListener('click', headerToggle);
+});
